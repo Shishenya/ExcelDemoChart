43d3dcef3a4b2c6a6a754507b1f69e7547bb8824
Sergey <s.shishenya@gmail.com>
2023-01-10 13:06:24 +0400

    project

---
+++ ChartTest/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ChartTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.LegendCellColumn legendCellColumn1 = new System.Windows.Forms.DataVisualization.Charting.LegendCellColumn();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chartExcel = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.excelComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCurrentState = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartExcel)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chartExcel
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartExcel.ChartAreas.Add(chartArea1);
+            legendCellColumn1.Name = "выаыва";
+            legend1.CellColumns.Add(legendCellColumn1);
+            legend1.Name = "Legend1";
+            legend1.Title = "График прибыли";
+            legend1.TitleSeparator = System.Windows.Forms.DataVisualization.Charting.LegendSeparatorStyle.Line;
+            this.chartExcel.Legends.Add(legend1);
+            this.chartExcel.Location = new System.Drawing.Point(445, 83);
+            this.chartExcel.Name = "chartExcel";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Прибыль";
+            this.chartExcel.Series.Add(series1);
+            this.chartExcel.Size = new System.Drawing.Size(562, 455);
+            this.chartExcel.TabIndex = 0;
+            this.chartExcel.Text = "chart1";
+            title1.Name = "title1";
+            this.chartExcel.Titles.Add(title1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1047, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.файлToolStripMenuItem.Text = "Меню";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel|*.xlsx";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(318, 396);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.excelComboBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(46, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 455);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные из файла";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(24, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(318, 396);
+            this.panel1.TabIndex = 5;
+            // 
+            // excelComboBox
+            // 
+            this.excelComboBox.FormattingEnabled = true;
+            this.excelComboBox.Location = new System.Drawing.Point(120, 25);
+            this.excelComboBox.Name = "excelComboBox";
+            this.excelComboBox.Size = new System.Drawing.Size(214, 21);
+            this.excelComboBox.TabIndex = 4;
+            this.excelComboBox.SelectedIndexChanged += new System.EventHandler(this.excelComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Активный лист:";
+            // 
+            // labelCurrentState
+            // 
+            this.labelCurrentState.AutoSize = true;
+            this.labelCurrentState.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCurrentState.Location = new System.Drawing.Point(51, 39);
+            this.labelCurrentState.Name = "labelCurrentState";
+            this.labelCurrentState.Size = new System.Drawing.Size(39, 20);
+            this.labelCurrentState.TabIndex = 4;
+            this.labelCurrentState.Text = "-----";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 578);
+            this.Controls.Add(this.labelCurrentState);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chartExcel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Демо: График из Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartExcel)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartExcel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox excelComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCurrentState;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ChartTest/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChartTest.Enums;
+using ExcelDataReader;
+
+namespace ChartTest
+{
+    public partial class Form1 : Form
+    {
+
+        private string _fileName = string.Empty;
+        private DataTableCollection _tableExcelCollection = null;
+        private CurrentState _currentState = CurrentState.noFileOpen;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    _fileName = openFileDialog1.FileName;
+                    // Text = _fileName;
+
+                    OpenExcelFile(_fileName);
+                    _currentState = CurrentState.yesFileOpen;
+                    UpdateLabelState();
+
+                }
+                else
+                {
+                    throw new Exception("Вы не выбрали файл!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка открытия файла", MessageBoxButtons.OK);
+                _currentState = CurrentState.noFileOpen;
+            }
+        }
+
+        private void OpenExcelFile(string pathFile)
+        {
+            FileStream stream = File.Open(pathFile, FileMode.Open, FileAccess.Read);
+
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+
+            DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+
+            _tableExcelCollection = db.Tables;
+            excelComboBox.Items.Clear();
+
+            // выводим список листов из файла в комбо бокс
+            foreach (DataTable dataTable in _tableExcelCollection)
+            {
+                excelComboBox.Items.Add(dataTable.TableName);
+            }
+
+            excelComboBox.SelectedIndex = 0;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateLabelState();
+        }
+
+        private void excelComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable table = _tableExcelCollection[Convert.ToString(excelComboBox.SelectedItem)];
+            dataGridView1.DataSource = table;
+
+            SetChartExcelPoints();
+        }
+
+        /// <summary>
+        /// Обновляет состояние текущего статуса
+        /// </summary>
+        private void UpdateLabelState()
+        {
+            switch (_currentState)
+            {
+                case CurrentState.noFileOpen:
+                    labelCurrentState.Text = "Для начала работы выберите файл в меню";
+                    break;
+                case CurrentState.yesFileOpen:
+                    labelCurrentState.Text = $"Вы работаете с файлом: {_fileName}";
+                    break;
+                default:
+                    labelCurrentState.Text = "";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Строит график прибыли
+        /// </summary>
+        private void SetChartExcelPoints()
+        {
+            chartExcel.Series[0].Points.Clear();
+            chartExcel.Titles[0].Text = Convert.ToString(excelComboBox.SelectedItem);
+            for (int i = 1; i < dataGridView1.Rows.Count; i++)
+            {
+                if (!Int32.TryParse(dataGridView1[0, i].Value.ToString(), out int x))
+                {
+                    continue;
+                }
+                else
+                {
+                    float y = float.Parse(dataGridView1[1, i].Value.ToString());
+
+                    chartExcel.Series[0].Points.AddXY(x, y);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Кнопка выхода
+        /// </summary>
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Выйти из программы?", "Выход", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
